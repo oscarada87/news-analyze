f0d550eef6196c92ebf5b2eef421bcdc8b22cd7b
Oscar <oscarada87@gmail.com>
2018-10-08 00:10:37 +0800

    範例程式碼完成

--- searchID.py
+++ searchID.py
@@ -6,22 +6,25 @@ import requests
 import time
 import json
 import re
+import abc
 
-class Crawler():
-    def __init__(self, url):
+class Crawler(abc.ABC):
+    def __init__(self, url, keyWord):
         self.url = url
-        self.urlList = []
+        self.keyWord = keyWord
         self.news = []
         self.driver = webdriver.Chrome(r"./driver/chromedriver.exe")
 
+    def GetNews(self):
+        return NotImplemented
 
 class IDCrawler(Crawler):
     def __init__(self, keyWord):
-        super(IDCrawler, self).__init__("https://www.inside.com.tw/")
-        self.keyWord = keyWord
+        super(IDCrawler, self).__init__("https://www.inside.com.tw/", keyWord)
         self.keyUrl = ""
         self.singleNewsSoup = None
         self.singleNews = {'title':'', 'time':'', 'content':'', 'resource':'', 'url':''}
+        self.urlList = []
         self.InitDriver()
 
     def InitDriver(self):
@@ -53,16 +56,20 @@ class IDCrawler(Crawler):
         self.singleNewsSoup = BeautifulSoup(res.text, 'html.parser')
 
     def GetTitle(self):
-        pass
+        self.singleNews['title'] = self.singleNewsSoup.find('h1',class_='post_header_title').get_text().strip()
 
     def GetTime(self):
-        pass
+        self.singleNews['time'] = self.singleNewsSoup.find('li',class_='post_date').find('span').get_text()
 
     def GetContent(self):
-        pass
+        temp = self.singleNewsSoup.find('div',class_='post_content article').find_all('p')
+        article = ''
+        for i in temp:
+            article = article + i.get_text()
+        self.singleNews['content'] = article
 
     def GetResource(self):
-        pass
+        self.singleNews['resource'] = self.singleNewsSoup.find('span',class_='post_author').find('a').get_text()
 
     def CrawlAllNews(self):
         count = 1
@@ -82,12 +89,15 @@ class IDCrawler(Crawler):
         self.GetNewsUrls()
         # pprint(self.urlList)
         self.CrawlAllNews()
-        # pprint(self.news)
+        pprint(self.news)
 
 def main():
     keyWord = input("請輸入關鍵字: ")
     CT = IDCrawler(keyWord)
-    CT.Start()
+    try:
+        CT.Start()
+    except:
+        pass
     time.sleep(100)
 
 main()
